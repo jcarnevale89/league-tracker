e0637cf78bec371d2f1ad95b9530680e0a3c8cb7
Jake Carnevale <jake.carnevale@realtruck.com>
2022-08-25 13:43:06 -0500

    fix stuff

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue/blob/44ff0e02cd0fd08b8cd7dee0127dbb5590446323/docs/user-guide/README.md#conflict-with-prettier
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  rules: {},
+}
--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="fixed inset-0 bg-black">
+    <div class="absolute inset-0 bg-cover bg-no-repeat bg-center bg-[url('/bg.jpg')] blur"></div>
+    <div class="absolute inset-0 bg-black bg-opacity-60 flex justify-center items-center">
+      <div>
+        <h2 class="text-4xl tracking-tight font-bold text-white sm:text-5xl lg:text-6xl">League Tracker</h2>
+        <p class="mt-5 text-xl text-gray-400">Coming Soon...</p>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
